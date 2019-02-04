b96e9265d2b55b1232198c7ae99d06f8ee100577
spar1993 <Spar1993@users.noreply.github.com>
2019-02-04 11:42:23 -0400

    Added source file

--- Lab 1.cpp
+++ Lab 1.cpp
@@ -1,1 +1,24 @@
-frb
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+string str1="hello";
+string str2= str1 + "world";
+// printting 
+cout <<"str1 = " <<str1<<endl;
+cout <<"the 4th character is" << str1[3] <<endl;
+cout <<"str1 has "<< str1.size()<<" characters"<<endl;
+// comparison coperators
+if (str1 == "hello")
+cout <<"hi there"<<endl;
+
+if(str2!= "world")
+cout<<"out of this world"<<endl;
+//appending to a string
+str2+= "ly one"; // or str2 = str2 + "ly one!!"
+
+}
+
